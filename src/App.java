5f825fe0a10333935f94f53ace27ba3914421e22
pedroDev <playstarpedro@gmail.com>
2024-07-17 10:59:16 -0300

    validando opções

--- src/App.java
+++ src/App.java
@@ -13,5 +13,40 @@ public class App {
         String opcao = JOptionPane.showInputDialog(null,
                 "Digite 1 para cadastro, 2 para consultar, 3 para exclusão, 4 para alteração ou 5 para sair",
                 "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+
+        // Loop da mensagem para opções invalidas
+        while (!isOpcaoValida(opcao)) {
+            if ("".equals(opcao)) {
+                sair();
+            }
+            opcao = JOptionPane.showInputDialog(null,
+                    "OPÇÃO INVÁLIDA! Digite 1 para cadastro, 2 para consultar, 3 para exclusão, 4 para alteração ou 5 para sair",
+                    "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        while (isOpcaoValida(opcao)) {
+
+        }
     }
+
+    // Opcao sair, fecha a aplicação
+    private static void sair() {
+        JOptionPane.showMessageDialog(null, "Até logo", "Sair", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+
+    private static boolean isOpcaoValida(String opcao) {
+        if ("1".equals(opcao) || "2".equals(opcao) || "3".equals(opcao) || "4".equals(opcao) || "5".equals(opcao)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isOpcaoCadastro(String opcao) {
+        if ("1".equals(opcao)) {
+            return true;
+        }
+        return false;
+    }
+
 }
