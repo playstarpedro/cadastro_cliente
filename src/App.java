582e1a9c6db722071f347adc25004946d2cf2ac2
pedroDev <playstarpedro@gmail.com>
2024-07-17 10:36:34 -0300

    criando App com o swing

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import br.com.psouza.dao.ClienteMapDAO;
+import br.com.psouza.dao.IClienteDAO;
+
+import javax.swing.*;
+
+public class App {
+
+    private static IClienteDAO iClienteDAO;
+
+    public static void main(String args[]) {
+        iClienteDAO = new ClienteMapDAO();
+
+        String opcao = JOptionPane.showInputDialog(null,
+                "Digite 1 para cadastro, 2 para consultar, 3 para exclusão, 4 para alteração ou 5 para sair",
+                "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
